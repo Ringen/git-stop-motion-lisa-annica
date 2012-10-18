75e50d47b564afa6691c3b15f2a590d8260daa07
annicalindstrom <annicalindstr_m@hotmail.com>
2012-10-18 11:39:28 +0200

    Can start the camera intent and save the captured image in a specific location. Sets the image in an imageview.

--- StopMotion/gen/se/lisaannica/stopmotion/R.java
+++ StopMotion/gen/se/lisaannica/stopmotion/R.java
@@ -15,11 +15,12 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int button_add=0x7f070000;
-        public static final int button_finish=0x7f070001;
-        public static final int button_save=0x7f070003;
-        public static final int editText_title=0x7f070002;
-        public static final int menu_create_new=0x7f070004;
+        public static final int button_add=0x7f070001;
+        public static final int button_finish=0x7f070002;
+        public static final int button_save=0x7f070004;
+        public static final int editText_title=0x7f070003;
+        public static final int image=0x7f070000;
+        public static final int menu_create_new=0x7f070005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
